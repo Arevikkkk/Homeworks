edc4a3a582db015855b95e84d4869393c1bd4ac3
Arevikkkk <50268266+Arevikkkk@users.noreply.github.com>
2019-08-02 17:15:39 +0400

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,3 +1,17 @@
+/* Task1 */
+
+function removeFirstElement(arr) {
+    if (!arr.length) {
+        return [];
+    } else {
+        arr[0] = removeFirstElement(arr[1]);
+        return arr;
+    }
+}
+
+let array = [6, 78, 'n', 0, 1];
+console.log(removeFirstElement(array)); // arajin elementy chi jnjum, 0-a sarqum(
+
 /* Task3 */
 
 function sumOfDigits(num) {
